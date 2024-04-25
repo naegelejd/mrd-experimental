4262755a818358146698689f6123ae4866869eb5
Joe Naegele <v-jnaegele@microsoft.com>
2024-04-25 15:30:55 +0000

    Increase macosx-version-min in conda build

--- cpp/conda/build.sh
+++ cpp/conda/build.sh
@@ -5,6 +5,10 @@ set -euo pipefail
 mkdir -p build-conda
 cd build-conda
 
+if [[ $(uname) == Darwin ]]; then
+    export CXXFLAGS="${CXXFLAGS} -isysroot ${CONDA_BUILD_SYSROOT} -mmacosx-version-min=${MACOSX_DEPLOYMENT_TARGET}"
+fi
+
 cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${PREFIX} \
       ../
 
