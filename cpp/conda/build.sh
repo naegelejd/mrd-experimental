a20aefdbee6069dbcc374e71ce99c2077e1f9f28
Joe Naegele <v-jnaegele@microsoft.com>
2024-04-25 18:34:24 +0000

    Export SDKROOT for osx conda build

--- cpp/conda/build.sh
+++ cpp/conda/build.sh
@@ -8,6 +8,7 @@ cd build-conda
 if [[ $(uname) == Darwin ]]; then
     export CFLAGS="${CFLAGS:-} -isysroot ${CONDA_BUILD_SYSROOT} -mmacosx-version-min=${MACOSX_DEPLOYMENT_TARGET}"
     export CXXFLAGS="${CXXFLAGS:-} -isysroot ${CONDA_BUILD_SYSROOT} -mmacosx-version-min=${MACOSX_DEPLOYMENT_TARGET}"
+    export SDKROOT="${CONDA_BUILD_SYSROOT}"
 fi
 
 echo 'Building mrd conda package using CMake...'
