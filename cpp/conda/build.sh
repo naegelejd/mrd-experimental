2b0941e5c9365d2ec02935cacd53f3269bb8e3d2
Joe Naegele <v-jnaegele@microsoft.com>
2024-04-25 18:00:37 +0000

    Hmm add OSX CFLAGS as well

--- cpp/conda/build.sh
+++ cpp/conda/build.sh
@@ -6,8 +6,11 @@ mkdir -p build-conda
 cd build-conda
 
 if [[ $(uname) == Darwin ]]; then
+    export CFLAGS="${CFLAGS:-} -isysroot ${CONDA_BUILD_SYSROOT} -mmacosx-version-min=${MACOSX_DEPLOYMENT_TARGET}"
     export CXXFLAGS="${CXXFLAGS:-} -isysroot ${CONDA_BUILD_SYSROOT} -mmacosx-version-min=${MACOSX_DEPLOYMENT_TARGET}"
 fi
+
+echo 'Building mrd conda package using CMake...'
 
 cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${PREFIX} \
       ../
